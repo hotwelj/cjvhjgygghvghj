dda2986c53ca1bc0588b87738153a9ec388bf7bd
hotwelj <111898845+hotwelj@users.noreply.github.com>
2026-06-02 22:46:11 -0300

    Update password hash for najmani16

--- netlify/functions/auth.js
+++ netlify/functions/auth.js
@@ -3,7 +3,7 @@ const crypto = require("crypto");
 // Hashes de las contraseñas válidas
 const HASHES = [
   "3305fea36bd49dc0fd4504dff836853483b6c525d90ed8a559af593918fc6064", // usuario.gratis
-  "7fcd3df7dc2fff173c22ea6450ede4f11b3e27c7923816aab660d474e27df8c2", // najmani16
+  "a5a8098e19611e966bff4f8243043767ef99bfee85f6499d2e8da5062420d25f", // najmani16
   "41c0ebccb07e468d1067b90472cbe6e27ca2631436ce83a528e7a00fb066c6f7", // cabe.tiene.aura
   "ff8e5d99a11ddc294515bc779a5569b39dbf745517756ef8b3c4a275621398d5", // 32208Sn3
 ];
